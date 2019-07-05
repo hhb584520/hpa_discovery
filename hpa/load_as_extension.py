79a630cd85da6a7a4aa08db05a059e2bc9b0e9ca
Haibin Huang <haibin.huang@intel.com>
2019-07-05 06:55:06 +0000

    Add all hpa capabilities
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- hpa/load_as_extension.py
+++ hpa/load_as_extension.py
@@ -15,14 +15,44 @@ if __name__ == '__main__':
     )
     parsed_args = parser.parse_args()
 
-    flavor = ""
+    flavor = {
+        "vcpus": "2",
+        "ram": "2048",
+        "disk": "2048",
+        "swap": "2048",
+        "OS-FLV-EXT-DATA:ephemeral": "23"
+    }
     extra_specs = {
         "aggregate_instance_extra_specs:storage": "local_image",
         "capabilities:cpu_info:model": "Haswell",
         "hw:cpu_policy": "dedicated",
         "hw:cpu_thread_policy": "prefer"
     }
-    viminfo = ""
+    viminfo = {
+        "createTime": "2017-04-01 02:22:27",
+        "domain": "Default",
+        "name": "TiS_R4",
+        "password": "admin",
+        "tenant": "admin",
+        "type": "openstack",
+        "url": "http://128.224.180.14:5000/v3",
+        "userName": "admin",
+        "vendor": "WindRiver",
+        "version": "newton",
+        "vimId": "windriver-hudson-dc_RegionOne",
+        'cloud_owner': 'windriver-hudson-dc',
+        'cloud_region_id': 'RegionOne',
+        'cloud_extra_info': {
+            "ovsDpdk": {
+                "version": "v1",
+                "arch": "Intel64",
+                "libname": "dataProcessingAccelerationLibrary",
+                "libversion": "v12.1",
+            }
+        },
+        'insecure': 'True'
+    }
+
     data = [flavor, extra_specs, viminfo]
 
     mgr = extension.ExtensionManager(
