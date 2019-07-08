eff866ef14908d5b5ff43105de728ede82cb1ce6
Haibin Huang <haibin.huang@intel.com>
2019-07-08 05:26:56 +0000

    Add all test case
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- hpa/load_as_extension.py
+++ hpa/load_as_extension.py
@@ -16,18 +16,63 @@ if __name__ == '__main__':
     parsed_args = parser.parse_args()
 
     flavor = {
-        "vcpus": "2",
+        "vcpus": 2,
         "ram": "2048",
-        "disk": "2048",
-        "swap": "2048",
-        "OS-FLV-EXT-DATA:ephemeral": "23"
+        "disk": "2G",
+        "swap": False,
+        "OS-FLV-EXT-DATA:ephemeral": False
     }
-    extra_specs = {
-        "aggregate_instance_extra_specs:storage": "local_image",
-        "capabilities:cpu_info:model": "Haswell",
-        "hw:cpu_policy": "dedicated",
-        "hw:cpu_thread_policy": "prefer"
-    }
+    extra_specs = [
+        # HPA UT1: CPU-Pinning
+        {
+            "aggregate_instance_extra_specs:storage": "local_image",
+            "capabilities:cpu_info:model": "Haswell",
+            "hw:cpu_policy": "dedicated",
+            "hw:cpu_thread_policy": "prefer"
+        },
+        # HPA UT2: CPU-Topology
+        {
+            "aggregate_instance_extra_specs:storage": "local_image",
+            "capabilities:cpu_info:model": "Haswell",
+            "hw:cpu_sockets": "2",
+            "hw:cpu_cores": "4",
+            "hw:cpu_threads": "16"
+        },
+        # HPA UT3: mem_page_size
+        {
+            "aggregate_instance_extra_specs:storage": "local_image",
+            "capabilities:cpu_info:model": "Haswell",
+            "hw:mem_page_size": "large"
+        },
+        # HPA UT4: numa_nodes
+        {
+            "aggregate_instance_extra_specs:storage": "local_image",
+            "capabilities:cpu_info:model": "Haswell",
+            "hw:numa_nodes": "2",
+            "hw:numa_cpus.0": "0,1",
+            "hw:numa_cpus.1": "2,3,4,5",
+            "hw:numa_mem.0": "2048",
+            "hw:numa_mem.1": "2048"
+        },
+        # HPA UT5: instruction set
+        {
+            "aggregate_instance_extra_specs:storage": "local_image",
+            "capabilities:cpu_info:model": "Haswell",
+            "hw:capabilities:cpu_info:features": "avx,acpi"
+        },
+        # HPA UT6: pci passthrough
+        {
+            "aggregate_instance_extra_specs:storage": "local_image",
+            "capabilities:cpu_info:model": "Haswell",
+            "pci_passthrough:alias": "sriov-vf-intel-8086-15b3:4"
+        },
+        # HPA UT7: sriov-nic
+        {
+            "aggregate_instance_extra_specs:sriov_nic": "sriov-nic-intel-8086-15b3-physnet-1:1",
+            "capabilities:cpu_info:model": "Haswell"
+        }
+    ]
+
     viminfo = {
         "createTime": "2017-04-01 02:22:27",
         "domain": "Default",
@@ -53,18 +98,19 @@ if __name__ == '__main__':
         'insecure': 'True'
     }
 
-    data = [flavor, extra_specs, viminfo]
-
-    mgr = extension.ExtensionManager(
-        namespace='hpa.discovery',
-        invoke_on_load=True,
-        invoke_args=(parsed_args.width,),
-    )
-
     def get_hpa_capabilities(ext, data):
         return (ext.name, ext.obj.get_hpa_capabilities(data))
 
-    results = mgr.map(get_hpa_capabilities, data)
-    for name, value in results:
-        print(name)
-        print(value)
+    for extra_spec in extra_specs:
+        data = [flavor, extra_spec, viminfo]
+
+        mgr = extension.ExtensionManager(
+            namespace='hpa.discovery',
+            invoke_on_load=True,
+            invoke_args=(parsed_args.width,),
+        )
+
+        results = mgr.map(get_hpa_capabilities, data)
+        for name, value in results:
+            print(name)
+            print(value)
